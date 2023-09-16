75df954443e73eeda663b6da429cc30d924f7002
Shinryuzz <mshin1127@gmail.com>
2023-09-17 00:53:54 +0900

    feat: call custom hooks in Terminal comp.

--- src/app/_components/Terminal.tsx
+++ src/app/_components/Terminal.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import { FC, useEffect } from "react";
 import { useTerminal } from "../_hooks/useTerminal";
+import { useGetUsers } from "../_hooks/useGetUsers";
 
 const TerminalWrapeer: FC = () => {
   const id = "terminal";
   const { setup } = useTerminal({ id });
+  const { isError, isLoading, data, error } = useGetUsers();
+  console.log(data);
 
   useEffect(() => {
     setup(document);
   }, []);
 
-  return <div id="terminal" className="min-h-screen bg-darker text-green" />;
+  return <div id={id} className="min-h-screen bg-darker text-green" />;
 };
 
 export default TerminalWrapeer;
