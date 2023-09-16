3724c1ee38871abdc081278edecf71491ced00a7
KakinokiKanta <kanta12621@icloud.com>
2023-09-16 21:08:38 +0900

    update: cdコマンドの実装

--- src/app/_components/Terminal.tsx
+++ src/app/_components/Terminal.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { Terminal } from "xterm";
 
 type Props = {
@@ -8,6 +8,10 @@ type Props = {
 };
 
 const TerminalWrapeer: FC<Props> = ({ id, cols = 80, rows = 24 }) => {
+  // const [command, setCommand] = useState<string>("");
+  let command: string = "";
+  let currentDir = "\r\n";
+
   const createTerminal = (rows: number = 5, cols: number = 1): Terminal => {
     const term = new Terminal({ cursorBlink: true, cols, rows });
     term.open(document.getElementById(id)!);
@@ -22,19 +26,42 @@ const TerminalWrapeer: FC<Props> = ({ id, cols = 80, rows = 24 }) => {
 
   const runFakeTerminal = (term: Terminal): void => {
     term.write("Welcome Term Park! \x1B[1;3;31mFake Terminal Now.\x1B[0m");
-    term.write("\r\n$ ");
+    term.write(currentDir + "$ ");
     term.onKey((e: { key: string; domEvent: KeyboardEvent }) => {
       const ev = e.domEvent;
       const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey;
 
       if (ev.key === "Enter") {
-        term.write("\r\n$ ");
+        const text: string[] = command.split(" ", 2);
+        const dir = "dir"; // TODO api呼び出しに変更
+        if (text[0] === "cd") {
+          if (text[1] !== dir) {
+            term.write(`\r\ncd: ${text[1]}: No such file or directory`);
+            term.write(currentDir + "$ ");
+          } else if (text[1] === dir) {
+            currentDir = `${currentDir}${text[1]} `;
+            term.write(currentDir + "$ ");
+          } else {
+            term.write(currentDir + "$ ");
+          }
+        } else {
+          term.write(
+            `\r\n${text[0]}: Command not found.  Use 'help' to see the command list.`
+          );
+          term.write(currentDir + "$ ");
+        }
+        command = "";
       } else if (ev.key === "Backspace") {
         if (term.buffer.active.cursorX > 2) {
+          command = command.slice(0, -1);
           term.write("\b \b");
+          console.log(command);
         }
       } else if (printable) {
         term.write(e.key);
+        // setCommand(command + e.key);
+        command += e.key;
+        console.log(command);
       }
     });
   };
